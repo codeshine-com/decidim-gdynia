50c1e163e31fc9959a65d46f6259d073b1f1e300
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2021-08-20 14:01:44 +0200

    trello card: 165 - content blocks change

--- decidim-core-extended/app/decorators/decidim/core/cells/how_to_participate_cell_decorator.rb
+++ decidim-core-extended/app/decorators/decidim/core/cells/how_to_participate_cell_decorator.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Decidim::ContentBlocks::HowToParticipateCell.class_eval do
-
+  include Rails.application.routes.mounted_helpers
+  
   def show
     render :show_new
   end
-  
+
 end
