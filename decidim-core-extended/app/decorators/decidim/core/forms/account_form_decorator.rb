5512dc55fc9168239c7af9a5e47ceec8c8efafcf
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2021-08-27 15:38:20 +0200

    trello card: 170 - birth date validation

--- decidim-core-extended/app/decorators/decidim/core/forms/account_form_decorator.rb
+++ decidim-core-extended/app/decorators/decidim/core/forms/account_form_decorator.rb
@@ -10,6 +10,8 @@ Decidim::AccountForm .class_eval do
   attribute :district
   attribute :inhabitant_card_number
 
+  validates :birth_year, numericality: { only_integer: true, greater_than: Date.current.year - 120, less_than: Date.current.year + 1, allow_nil: true }, if: proc { |attr| attr[:birth_year].present? || !attr[:birth_year].nil? }
+
   def genders_for_select
     Decidim::User.const_get(:GENDERS).map do |text|
       [
