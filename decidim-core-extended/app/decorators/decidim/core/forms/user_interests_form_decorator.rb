3a5e6d88da07bcafcb4390955b207ae4326fe364
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2021-08-31 21:14:50 +0200

    small fixes

--- decidim-core-extended/app/decorators/decidim/core/forms/user_interests_form_decorator.rb
+++ decidim-core-extended/app/decorators/decidim/core/forms/user_interests_form_decorator.rb
@@ -4,7 +4,7 @@ Decidim::UserInterestsForm.class_eval do
   attribute :areas, Array[Decidim::UserInterestAreaForm]
 
   def map_model(user)
-    self.scopes = user.organization.scopes.top_level.promoted_first.map do |scope|
+    self.scopes = Decidim::Scope.where(decidim_organization_id: user.organization.id).top_level.promoted_first.map do |scope|
       Decidim::UserInterestScopeForm.from_model(scope: scope, user: user)
     end
     self.areas = Decidim::Area.where(decidim_organization_id: user.organization.id).order(area_type_id: :desc).map do |area|
