fb8857101598d63ae576df4db140eada8b7e0a54
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2021-08-31 22:08:51 +0200

    trello card: 188 - translation fixes

--- decidim-surveys_extended/app/decorators/forms/admin_answer_form_decorator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Decidim::Forms::Admin::AnswerOptionForm.class_eval do
-
-  # clear_validators!
-  # validates :body, translatable_presence: true, unless: proc { |form| form.deleted }
-
-end
--- decidim-surveys_extended/app/decorators/models/survey_decorator.rb
+++ decidim-surveys_extended/app/decorators/models/survey_decorator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Decidim::Surveys::Survey.class_eval do
 
   def private?
@@ -11,7 +13,6 @@ Decidim::Surveys::Survey.class_eval do
   def current_user_can_visit_survey?(user)
     return true if user&.admin
     return true if user && (component.participatory_space.user_roles(role_name = 'admin').pluck(:decidim_user_id).include? user.id)
-    return visible_for_unregistered unless user
 
     private? && !transparent? ? (user && component.participatory_space.participatory_space_private_users.exists?(decidim_user_id: user.id)) : true
   end
