808eaf1223f9c828ae63cca16b43a5f350582c64
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2021-09-03 00:07:58 +0200

    trello card: 191 - conversations changes

--- decidim-core-extended/app/assets/javascripts/decidim/core/extended/application.js
+++ decidim-core-extended/app/assets/javascripts/decidim/core/extended/application.js
@@ -33,3 +33,36 @@ $( function() {
     });
   }
 });
+
+
+$( function() {
+  // $('body').on('click', 'li', function(e) {
+  //   console.log('cokolwiek');
+  //   $('#user-conversations-add-modal').find('button[type="submit"]').addClass('disabled');
+  //   $('#user-conversations-add-modal').find('button[type="submit"]').attr('disabled', 'true');
+  // })
+
+  if ($('#user-conversations-add-modal').length > 0) {
+    // if ($('.js-multiple-mentions-recipients label').length == 0) {
+    //   $('#user-conversations-add-modal').find('button[type="submit"]').addClass('disabled');
+    //   $('#user-conversations-add-modal').find('button[type="submit"]').attr('disabled', 'true');
+    // }
+
+    $('body').on('mousedown', '#results li', function(e) {
+      $('#user-conversations-add-modal').find('button[type="submit"]').removeClass('disabled');
+      $('#user-conversations-add-modal').find('button[type="submit"]').attr('disabled', null);
+    });
+
+    $('body').on('mousedown', '.js-multiple-mentions-recipients label div', function(e) {
+      setTimeout( function() {
+        if ($('.js-multiple-mentions-recipients label').length > 0) {
+          $('#user-conversations-add-modal').find('button[type="submit"]').removeClass('disabled');
+          $('#user-conversations-add-modal').find('button[type="submit"]').attr('disabled', null);
+        } else {
+          $('#user-conversations-add-modal').find('button[type="submit"]').addClass('disabled');
+          $('#user-conversations-add-modal').find('button[type="submit"]').attr('disabled', 'true');
+        }
+      }, 200);
+    });
+  }
+});
