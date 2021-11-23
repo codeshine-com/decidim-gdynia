c6cc0b83579c2f90a12284e05a23a2e51f5c7b4a
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2021-11-23 16:35:28 +0100

    trello card: 234 - allowing private users commenting in meetings

--- decidim-module-meetings_extended/app/decorators/decidim/meetings/models/meeting_decorator.rb
+++ decidim-module-meetings_extended/app/decorators/decidim/meetings/models/meeting_decorator.rb
@@ -12,4 +12,29 @@ Decidim::Meetings::Meeting.class_eval do
     }
   end
 
+  def can_be_joined_by?(user)
+    !closed? && registrations_enabled? && !private_meeting? && can_participate?(user)
+    # !closed? && registrations_enabled? && on_this_platform? && can_participate?(user)
+  end
+
+  private
+
+  def can_participate_in_meeting?(user)
+    return true if private_meeting? && user_permitted_for_private_meeting_actions?(user)
+    return false unless user
+
+    registrations.exists?(decidim_user_id: user.id)
+  end
+
+  def user_permitted_for_private_meeting_actions?(user)
+    user.admin? || space_admin?(user) || private_user?(user)
+  end
+
+  def private_user?(user)
+    meeting.participatory_space.participatory_space_private_users.map(&:decidim_user_id).include?(user.id)
+  end
+
+  def space_admin?(user)
+    meeting.participatory_space.user_roles.map(&:decidim_user_id).include?(user.id)
+  end
 end
