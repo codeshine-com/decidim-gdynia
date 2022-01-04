c467d712c4a2a08e66a5357c05c6fce2d11c2f3a
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2022-01-04 14:51:59 +0100

    trello card: 237 - dates change

--- decidim-processes-extended/app/decorators/helpers/participatory_processes_helper_decorator.rb
+++ decidim-processes-extended/app/decorators/helpers/participatory_processes_helper_decorator.rb
@@ -3,10 +3,10 @@
 Decidim::ParticipatoryProcesses::ParticipatoryProcessHelper.class_eval do
   def step_dates(participatory_process_step)
     if participatory_process_step.start_date == participatory_process_step.end_date && participatory_process_step.start_date.present?
-      localize(participatory_process_step.start_date.to_date, format: :default)
+      localize(participatory_process_step.start_date.to_date, format: :decidim_short)
     else
       dates = [participatory_process_step.start_date, participatory_process_step.end_date]
-      dates.map { |date| date ? localize(date.to_date, format: :default) : "?" }.join(" - ")
+      dates.map { |date| date ? "<strong>#{localize(date.to_date, format: :decidim_short)}</strong>" : "<strong>?</strong>" }.join(" - ").html_safe
     end
   end
 end
