247f9e8eaacec50b961a9660b3a0364f09282790
Przemysław Adamowicz <przemad@gmail.com>
2024-01-25 07:05:05 +0100

    add missing admin translation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  if Rails.env.staging?
+    require 'sidekiq/web'
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+  end
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
