99a5df32f7b56893da42994e0db893b4684f25d2
Przemysław Adamowicz <przemad@gmail.com>
2024-01-26 10:31:04 +0100

    register assets packs for problems with Zeitwerk

--- decidim-module-meetings_extended/lib/decidim/meetings_extended/engine.rb
+++ decidim-module-meetings_extended/lib/decidim/meetings_extended/engine.rb
@@ -30,6 +30,10 @@ module Decidim
           require_dependency(c)
         end
       end
+
+      initializer "module_meetings_extended.webpacker.assets_path" do
+        Decidim.register_assets_path File.expand_path("app/packs", root)
+      end
     end
   end
 end
--- decidim-pages_extended/lib/decidim/pages_extended/engine.rb
+++ decidim-pages_extended/lib/decidim/pages_extended/engine.rb
@@ -40,6 +40,10 @@ module Decidim
           require_dependency(c)
         end
       end
+
+      initializer "pages_extended.webpacker.assets_path" do
+        Decidim.register_assets_path File.expand_path("app/packs", root)
+      end
     end
   end
 end
