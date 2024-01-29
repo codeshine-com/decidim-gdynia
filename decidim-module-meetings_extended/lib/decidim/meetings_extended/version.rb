0eb797500f63a9dfb9e3eda26310f8a76c78a187
Przemysław Adamowicz <przemad@gmail.com>
2024-01-29 21:34:05 +0100

    update version to 0.26.9

--- decidim-module-meetings_extended/lib/decidim/meetings_extended/version.rb
+++ decidim-module-meetings_extended/lib/decidim/meetings_extended/version.rb
@@ -4,6 +4,6 @@ module Decidim
   # This holds the decidim-meetings version.
   module MeetingsExtended
     VERSION = '0.1.0'
-    DECIDIM_VERSION = "0.26.8"
+    DECIDIM_VERSION = "0.26.9"
   end
 end
--- decidim-proposals_extended/lib/decidim/proposals_extended/version.rb
+++ decidim-proposals_extended/lib/decidim/proposals_extended/version.rb
@@ -1,6 +1,6 @@
 module Decidim
   module ProposalsExtended
     VERSION = '0.1.0'
-    DECIDIM_VERSION = "0.26.8"
+    DECIDIM_VERSION = "0.26.9"
   end
 end
