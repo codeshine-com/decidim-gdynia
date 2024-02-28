5f65bbb446e1dfe54db0a17ae2c8bf81baf2c83f
Przemysław Adamowicz <przemad@gmail.com>
2024-02-28 01:18:04 +0100

    test public activity search change

--- decidim-processes-extended/app/decorators/services/participatory_process_search_decorator.rb
+++ decidim-processes-extended/app/decorators/services/participatory_process_search_decorator.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
-Decidim::ParticipatoryProcesses::ParticipatoryProcessSearch.class_eval do
+# TODO: do aktualizacji - usuniete w 0.27
 
-  def search_scope_id
-    clean_scope_ids = if scope_id.is_a?(Hash)
-                        scope_id.values
-                      else
-                        [scope_id].flatten
-                      end
-
-    if clean_scope_ids.any?
-      query.joins(:participatory_space_resource_links_from).where('decidim_participatory_space_links.to_type': 'Decidim::Scope').where('decidim_participatory_space_links.to_id': clean_scope_ids)
-    else
-      query
-    end
-  end
-end
+# Decidim::ParticipatoryProcesses::ParticipatoryProcessSearch.class_eval do
+#
+#   def search_scope_id
+#     clean_scope_ids = if scope_id.is_a?(Hash)
+#                         scope_id.values
+#                       else
+#                         [scope_id].flatten
+#                       end
+#
+#     if clean_scope_ids.any?
+#       query.joins(:participatory_space_resource_links_from).where('decidim_participatory_space_links.to_type': 'Decidim::Scope').where('decidim_participatory_space_links.to_id': clean_scope_ids)
+#     else
+#       query
+#     end
+#   end
+# end
