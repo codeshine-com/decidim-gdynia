1960236177e8eeb169df58e8a4e08b85c9a41368
Przemysław Adamowicz <przemad@gmail.com>
2024-03-01 09:48:16 +0100

    fix starting conversations

--- decidim-core-extended/app/decorators/decidim/core/commands/create_registration_decorator.rb
+++ decidim-core-extended/app/decorators/decidim/core/commands/create_registration_decorator.rb
@@ -34,10 +34,10 @@ Decidim::CreateRegistration.class_eval do
       nickname: form.nickname,
       password: form.password,
       password_confirmation: form.password_confirmation,
+      password_updated_at: Time.current,
       organization: form.current_organization,
       tos_agreement: form.tos_agreement,
       newsletter_notifications_at: form.newsletter_at,
-      email_on_notification: true,
       accepted_tos_version: form.current_organization.tos_version,
       locale: form.current_locale,
       # custom
