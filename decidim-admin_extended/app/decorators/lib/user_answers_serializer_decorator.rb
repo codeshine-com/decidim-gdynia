7e914e8141f8711ca7ebd880315cce0af8be9444
Przemysław Adamowicz <przemad@gmail.com>
2025-03-31 16:55:12 +0200

    fix/reformat date in surveys export

--- decidim-admin_extended/app/decorators/lib/user_answers_serializer_decorator.rb
+++ decidim-admin_extended/app/decorators/lib/user_answers_serializer_decorator.rb
@@ -44,6 +44,16 @@ Decidim::Forms::UserAnswersSerializer.class_eval do
 
   private
 
+  def hash_for(answer)
+    {
+      answer_translated_attribute_name(:id) => answer&.session_token,
+      # answer_translated_attribute_name(:created_at) => answer&.created_at,
+      answer_translated_attribute_name(:created_at) => answer&.created_at&.strftime('%d/%m/%Y: %H:%M'),
+      answer_translated_attribute_name(:ip_hash) => answer&.ip_hash,
+      answer_translated_attribute_name(:user_status) => answer_translated_attribute_name(answer&.decidim_user_id.present? ? "registered" : "unregistered")
+    }
+  end
+
   def questions_hash
     questionnaire_id = @answers.first&.decidim_questionnaire_id
     return {} unless questionnaire_id
