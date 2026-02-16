9f828b0e5284dc5850e54b34d73cb44139369f07
radoslawmilner <radoslaw.milner@codeshine.com>
2026-02-16 12:40:35 +0100

    refactor LIS-92 due to moved decorators

--- decidim-module-gdynia/app/decorators/decidim/cells/participatory_space_metadata_cell_decorator.rb
+++ decidim-module-gdynia/app/decorators/decidim/cells/participatory_space_metadata_cell_decorator.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 Decidim::ContentBlocks::ParticipatorySpaceMetadataCell.class_eval do
-
+  # overwritten method
+  # rebuild method - drop decidim_escape_translated
+  # translate to PL value for const attr created_by
   def metadata_valued_items
     metadata_items.filter_map do |item|
-      next if (decidim_escape_translated(presented_space.send(item))).blank?
+      raw_value = presented_space.send(item)
+      value = if item == "created_by" && raw_value.present?
+                I18n.t("decidim.assemblies.created_by.#{raw_value}", default: raw_value)
+              else
+                translated_attribute(raw_value)
+              end
+      next if value.blank?
 
-      value = translated_attribute(presented_space.send(item))
       {
         title: t(item, scope: translations_scope),
         value:
       }
     end
   end
-
 end
-
